c18122f10998b6be1ef1f885a934a5b4199b8ae2
livenb <livenb666@gmail.com>
2016-10-16 18:53:56 +0000

    update

--- code/model_tuning.py
+++ code/model_tuning.py
@@ -157,10 +157,10 @@ def xgb_params_search(X, y):
 
 def run_grid_search(X, y):
     params = {'learning_rate': [0.1],
-              'gamma': [0, 0.05, 0.1, 0.5, 1, 1.5, 3, 5, 7, 10],
+              'gamma': [0],
               'max_depth': [6],
               'min_child_weight': [1],
-              'subsample': [0.7],
+              'subsample': [0.5, 0.6, 0.7, 0.8, 0.9, 1],
               'colsample_bytree': [0.8],
               'colsample_bylevel': [0.85],
               }
